f2e174661463d2996c7119c1e3b15d4c9c59f369
hoon-hoon <kc0393@naver.com>
2024-04-17 19:02:00 +0900

    [feat] marker 상태관리 및 이벤트

--- src/shared/components/StyledInfoWindow.tsx
+++ src/shared/components/StyledInfoWindow.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import kakaoIcon from '@shared/assets/kakaoMapIcon.png';
 import naverIcon from '@shared/assets/naverMapIcon.png';
 import mockImg from '@shared/assets/photo.png';
 import Image from 'next/image';
-
-interface StyledInfoWindowProps {
-    name: string;
-    description: string;
-}
+import { Colors } from '@shared/constants';
+import { MarkerInfo } from '@shared/types';
 
 const InfoWindowContainer = styled.div`
   display: flex;
@@ -16,10 +13,9 @@ const InfoWindowContainer = styled.div`
   border-radius: 4px;
   min-width: 295px;
   max-width: 116px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   padding: 8px;
   border: 1px solid #ccc;
-  z-index: 1;
+  z-index: 3;
 `;
 
 const LeftSection = styled.div`
@@ -36,13 +32,13 @@ const RightSection = styled.div`
 `
 
 const InfoTitle = styled.h1`
-  color: #2E2E2F;
+  color: ${Colors.Black900};
   font-size: 18px;
   margin: 0 0 5px 0;
 `;
 
 const InfoDescription = styled.div`
-  color: #5C5C5D;
+  color: ${Colors.Black800};
   font-size: 12px;
   margin-bottom: 10px;
 `;
@@ -53,12 +49,13 @@ const ActionsContainer = styled.div`
   margin-top: 10px;
 `;
 
-const AddButton = styled.button`
+const AddButton = styled.button.attrs(props => ({
+}))<AddButtonProps>`
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 8px;
-  background-color: #03251A;
+  background-color: ${props => props.added ? Colors.Emerald900 : Colors.Emerald500};
   color: #FFFFFF;
   border: none;
   border-radius: 4px;
@@ -80,34 +77,62 @@ const IconImageWrapper = styled.div`
   margin-right: 8px;
 `;
 
-const StyledInfoWindow: React.FC<StyledInfoWindowProps> = ({ name, description }) => {
-    return (
-        <InfoWindowContainer>
-            <LeftSection>
-                <ImgContainer>
-                <Image src={mockImg}/>
-                </ImgContainer>
-            </LeftSection>
-            <RightSection>
-                <InfoTitle>{name}</InfoTitle>
-                <InfoDescription>{description}</InfoDescription>
-                <ActionsContainer>
-                    <IconWrapper>
-                        <IconImageWrapper>
-                            <Image src={kakaoIcon} alt="카카오맵" width={24} height={24} />
-                        </IconImageWrapper>
-                        <IconImageWrapper>
-                            <Image src={naverIcon} alt="네이버맵" width={24} height={24} />
-                        </IconImageWrapper>
-                    </IconWrapper>
-                    <AddButton onClick={() => alert(`${name} 추가하기`)}>
-                        추가하기&emsp;+ 
-                    </AddButton>
-                </ActionsContainer>
-            </RightSection>
+interface StyledInfoWindowProps extends Partial<MarkerInfo> {
+  onToggleAdded: () => void;
+  overlayRef: React.RefObject<kakao.maps.CustomOverlay>;
+}
 
-        </InfoWindowContainer>
-    );
+interface AddButtonProps {
+  added: boolean;
+}
+
+const StyledInfoWindow: React.FC<StyledInfoWindowProps> = ({ name, description, added, onToggleAdded, overlayRef }) => {
+  const addedMarkers = JSON.parse(localStorage.getItem('addedMarkers') || '[]');
+  const [localAdded, setLocalAdded] = useState(added);
+
+
+  useEffect(() => {
+    if (overlayRef && overlayRef.current) {
+      const content = overlayRef.current.getContent();
+      if (content instanceof HTMLElement) {
+        content.style.display = localAdded ? 'block' : 'none';
+      }
+    }
+  }, [localAdded, overlayRef]);
+  
+
+  const handleButtonClick = () => {
+    onToggleAdded();
+    setLocalAdded(current => !current);
+  };
+
+  return (
+    <InfoWindowContainer>
+      <LeftSection>
+        <ImgContainer>
+          <Image src={mockImg} />
+        </ImgContainer>
+      </LeftSection>
+      <RightSection>
+        <InfoTitle>{name}</InfoTitle>
+        <InfoDescription>{description}</InfoDescription>
+        <ActionsContainer>
+          <IconWrapper>
+            <IconImageWrapper>
+              <Image src={kakaoIcon} alt="카카오맵" width={24} height={24} />
+            </IconImageWrapper>
+            <IconImageWrapper>
+              <Image src={naverIcon} alt="네이버맵" width={24} height={24} />
+            </IconImageWrapper>
+          </IconWrapper>
+          <AddButton onClick={handleButtonClick} added={localAdded!}>
+            {localAdded ? '삭제하기' : '추가하기'}&emsp;{localAdded ? 'x' : '+'}
+          </AddButton>
+        </ActionsContainer>
+      </RightSection>
+
+    </InfoWindowContainer>
+  );
 };
 
 export default StyledInfoWindow;

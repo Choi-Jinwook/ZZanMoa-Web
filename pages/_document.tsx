d268ec2348284ccde382eab7f6b96a7c35145d88
최진욱 <jinwook.fe@gmail.com>
2024-04-29 18:48:40 +0900

    [test] 배포환경 테스트

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
         />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
       </Head>
       <body>
         <Main />
